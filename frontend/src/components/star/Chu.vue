944d257efbe50e862ebbdd1ed7814161890a2759
sochic2 <netzzang12@gmail.com>
2019-10-21 09:33:49 +0900

    make_router

---
+++ frontend/src/components/star/Chu.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>추천</h1>
+</template>
+
+<script>
+
+export default {
+  name:'Menu',
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+
+
+  },
+  methods: {
+
+
+  }
+}
+</script>
+
+<style>
+</style>
