1c8aeeb0314fa341d07ff6e0f67bddfb590fce2a
hongyong3 <chy@naver.com>
2019-10-25 17:53:13 +0900

    Google_Login

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,28 +3,14 @@ import Router from 'vue-router'
 import Frame from './components/star/Frame.vue'
 import Chu from './components/star/Chu.vue'
 import Detail from './components/star/Detail.vue'
-<<<<<<< HEAD
-import Login from './components/login/Login.vue'
-
-=======
 import MakeCombi from './components/star/MakeCombi.vue'
->>>>>>> cc27bdf9dd2ac7fef9a131df9973021cb8f138c3
 Vue.use(Router)
 
 export default new Router({
 
   mode: 'history',
   base: process.env.BASE_URL,
-<<<<<<< HEAD
   routes: [{
-      path: '/menu',
-      name: 'menu',
-      component: Menu
-    },
-=======
-  routes: [
->>>>>>> cc27bdf9dd2ac7fef9a131df9973021cb8f138c3
-    {
       path: '/Starbucks',
       name: 'Frame',
       component: Frame
@@ -41,17 +27,10 @@ export default new Router({
       props: true
     },
     {
-<<<<<<< HEAD
-      path: 'Login',
-      name: 'Login',
-      component: Login,
-    },
-=======
       path: '/MakeCombi',
       name: 'MakeCombi',
       component: MakeCombi
     }
 
->>>>>>> cc27bdf9dd2ac7fef9a131df9973021cb8f138c3
   ]
 })
