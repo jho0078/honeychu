f65d9776ad5f4864d6dcba7527e75362e8af6bd0
sochic2 <netzzang12@gmail.com>
2019-10-28 16:35:08 +0900

    make_admin_router

---
+++ frontend/src/components/admin/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>안녕</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Admin',
+  components:{
+
+  },
+  data() {
+    return {
+      
+  }
+},
+  methods: {
+
+  },
+}
+</script>
+
+<style>
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,6 +5,7 @@ import Chu from './components/star/Chu.vue'
 import Detail from './components/star/Detail.vue'
 import MakeCombi from './components/star/MakeCombi.vue'
 import NamkiPractice from './components/star/NamkiPractice.vue'
+import Admin from './components/admin/Admin.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -42,6 +43,11 @@ export default new Router({
       path: '/NamkiPractice',
       name: 'NamkiPractice',
       component: NamkiPractice
+    },
+    {
+      path: '/Admin',
+      name: 'Admin',
+      component: Admin
     }
 
   ]
