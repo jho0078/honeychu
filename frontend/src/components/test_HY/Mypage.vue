648cfccd692ccd80de240de849086fe0b229bbbb
hongyong3 <chy@naver.com>
2019-10-28 17:58:48 +0900

    Mypage

--- frontend/src/components/test_HY/Mypage.vue
+++ frontend/src/components/test_HY/Mypage.vue
@@ -1,15 +1,52 @@
 <template>
   <div id="app">
-    <h1>Mypage</h1>
+    <div>
+      <h1>Mypage</h1>
+      <hr />
+    </div>
+    <div>
+      <div class="Like_HY">
+        <button @click="showRc">
+          <h3>좋아요한메뉴</h3>
+        </button>
+      </div>
+
+      <div class="Mymenu_HY">
+        <button @click="showMenu">
+          <h3>내가 작성한 메뉴</h3>
+        </button>
+      </div>
+      <div v-if="!toggle2" v-show="toggle1">좋아요</div>
+      <div v-if="toggle1" v-show="toggle2">나의메뉴</div>
+    </div>
   </div>
 </template>
 
 <script>
+import "@/components/test_HY/Mypage.css";
 export default {
   name: "Mypage",
   data() {
-    return {};
+    return {
+      toggle1: false,
+      toggle2: false
+    };
   },
-  methods: {}
+  methods: {
+    showRc() {
+      (this.toggle1 = !toggle1), (this.toggle2 = false);
+    },
+    showMenu() {
+      (this.toggle2 = !toggle2), (this.toggle1 = false);
+    }
+    // toggle1Show() {
+    //   this.istoggle1 = !istoggle1;
+    //   this.istoogle2 = false;
+    // },
+    // toggle2Show() {
+    //   this.istoggle2 = !istoggle2;
+    //   this.istoogle1 = false;
+    // }
+  }
 };
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueRouter from 'vue-router'
-import GAuth from 'vue-google-oauth2'
+
 
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
-Vue.use(GAuth, {
-  clientId: '503235171379-32dsqd6es3f6pb1hjjpcrtikd7vv0s6m.apps.googleusercontent.com'
-})
 
 
 new Vue({
