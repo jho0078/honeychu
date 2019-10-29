a730d63c79994f3ccfcb9ca6e174bf7ef26af4ab
HSx3 <hs.ssafy@gmail.com>
2019-10-29 10:17:16 +0900

    add starcombi router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const starmenu = require('./starmenu')
+const starcombi = require('./starcombi')
 
 router.use('/starmenu', starmenu)
+router.use('/starcombi', starcombi)
 
 module.exports = router
