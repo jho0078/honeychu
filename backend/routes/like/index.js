c7f3eafd97bee992d7f1d6506e0c91d0c6f9f386
HSx3 <hs.ssafy@gmail.com>
2019-11-01 16:23:38 +0900

    like API

---
+++ backend/routes/like/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+const models = require('../../models');
+
+// 모든 유저 조회
+router.get('/', function(req, res) {
+    models.like.findAll().then((like) => {
+    res.json(like)
+  });
+});
+
+// 특정 유저 조회
+router.get('/user/:id', function(req, res) {
+    let userID = req.params.id
+    models.like.findAll({
+        where: {
+            like_user_id: userID
+        }
+    }).then((like) => {
+    res.json(like)
+  });
+});
+
+// 특정 메뉴 조회
+// count 추가 가능?
+router.get('/menu/:id', function(req, res) {
+    let starmenuID = req.params.id
+    models.like.findAll({
+        where: {
+            like_starmenu_id: starmenuID
+        }
+    }).then((like) => {
+    res.json(like)
+  });
+});
+
+
+module.exports = router;
