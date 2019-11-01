2e3db58f3242e713ab6bdf37a94a40850d3cb905
HSx3 <hs.ssafy@gmail.com>
2019-11-01 14:24:27 +0900

    user_check

--- backend/routes/user/index.js
+++ backend/routes/user/index.js
@@ -44,4 +44,45 @@ router.post('/', function(req, res) {
 // });
 
 
+// 로그인 POST
+router.post("/login", async function(req,res,next){
+  // let body = req.body;
+
+  let result = await models.user.findOne({
+    where: {
+      email : req.body.email
+    }
+  });
+
+  // let result = models.user.findOne({
+  //   where: {
+  //     email: req.body.email
+  //   }
+  // });
+
+  // let dbEmail = result.dataValues.email;
+  if(result){
+    console.log("회원입니다.")
+    res.redirect("http://localhost:8080/honeyChu")
+  }
+  else{
+    console.log("회원이 아닙니다.")
+    res.redirect("http://localhost:8080/honeyChu")
+    // res.redirect("")
+  }
+  // let dbPassword = result.dataValues.password;
+  // let inputPassword = body.password;
+  // let salt = result.dataValues.salt;
+  // let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+
+  // if(dbPassword === hashPassword){
+  //   console.log("비밀번호 일치");
+  //   res.redirect("/users");
+  // }
+  // else{
+  //   console.log("비밀번호 불일치");
+  //   res.redirect("/users/login");
+  // }
+});
+
 module.exports = router;
