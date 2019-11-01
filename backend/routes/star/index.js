3697ef83e7461007f6e8601c92348f4b929538ca
HSx3 <hs.ssafy@gmail.com>
2019-11-01 11:10:22 +0900

    user API

--- backend/routes/star/index.js
+++ backend/routes/star/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
 
-const user = require('../user')
 const starmenu = require('./menu')
 
-router.use('/', user)
 router.use('/menu', starmenu)
 
 module.exports = router
--- backend/routes/user/index.js
+++ backend/routes/user/index.js
@@ -4,13 +4,44 @@ const models = require('../../models');
 
 
 // 조회
-router.get('/', function(request, response) {
-    // models.starmenu.findAll().then((starmenu) => {
-    //     response.json(starmenu)
+router.get('/', function(req, res) {
     models.user.findAll().then((user) => {
-        response.json(user)
+        res.json(user)
   });
 });
 
+// 생성
+router.post('/', function(req, res) {
+  models.user.create({
+      user_id: req.body.user_id,
+      email: req.body.email
+    }).then((result) => {
+      res.json(result)
+  }, (validation) => {
+    res.json({
+      errores: validation.errors.map((error) => {
+        return {
+          attribute: error.path,
+          message: error.message
+        };
+      })
+    });
+    //TODO: error handling
+  });
+});
+
+// // 탈퇴
+// router.delete('/:id', function(req, res) {
+//   let menuID = req.params.id;
+//   models.starmenu.destroy({
+//     where: {starmenu_id: menuID}
+//   })
+//   .then( result => {
+//     res.redirect("/board")
+//   })
+//   .catch( err => {
+//     console.log("데이터 삭제 실패");
+//   });
+// });
 
 module.exports = router;
