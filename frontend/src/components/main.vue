1e33914c8b988c511941fc070d228ec56c9acb59
HSx3 <hs.ssafy@gmail.com>
2019-11-04 15:28:54 +0900

    login check

--- frontend/src/components/main.vue
+++ frontend/src/components/main.vue
@@ -13,7 +13,7 @@
 
 		<div class="hyeri_loginout"> 
 			<div id="kakao-login-btn"></div>
-  			<!-- <div class="hyeri_logout" @click="logout()">로그아웃</div> -->
+  			<div class="hyeri_logout" @click="logout()">로그아웃</div>
 		</div>
 	</div>
 </template>
@@ -30,7 +30,14 @@
 	    }
 	  },
 	  mounted() {	  	
-	  	this.kakao_login()
+		  this.kakao_login()
+		  Kakao.Auth.getStatus(function(statusObj) {
+						if (statusObj.status == "not_connected") {
+							console.log('xxxxx')
+						} else {
+							console.log('ooooo')
+						}
+					})
   	},
 
 	  
@@ -51,10 +58,17 @@
 		          	console.log('res',res)
 		            console.log('email :',res.kakao_account.email);
 		            console.log('id :', res.id);
-		            console.log('nickname :', res.properties.nickname);
+		            // console.log('nickname :', res.properties.nickname);
 		          	axios.post("/api/user",{email: res.kakao_account.email})
+		          	axios.post("/api/user/login",{email: res.kakao_account.email})
 		            alert('로그인 되었습니다.')
-		            
+		            Kakao.Auth.getStatus(function(statusObj) {
+						if (statusObj.status == "not_connected") {
+							console.log('xxxxx')
+						} else {
+							console.log('ooooo')
+						}
+					})
    		          },
 		          fail: function (error) {
 		            alert(JSON.stringify(error));
@@ -70,7 +84,21 @@
 	      this.$router.push({name:'Starbucks'})
 	    },
 	    logout(){
+			Kakao.Auth.logout(function () {
+				setTimeout(function(){
+					location.href="http://localhost:8080"
+				}, 1000);
+		
+				})
+			Kakao.Auth.getStatus(function(statusObj) {
+						if (statusObj.status == "not_connected") {
+							console.log('xxxxx')
+						} else {
+							console.log('ooooo')
+						}
+					})
 	    	this.username='';
+			
 		}
 	}
 }
