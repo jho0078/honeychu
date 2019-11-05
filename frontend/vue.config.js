7d9520519c7f3859c21fa9944bd51f871a5e069d
정세민 <jho0078@nate.com>
2019-11-05 17:51:09 +0900

    vue config proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/api': {
-          target: 'http://localhost:3000/',
+          target: 'http://52.78.224.61:3000/',
           changeOrigin: true,
           pathRewrite: {
             '^/api': ''
